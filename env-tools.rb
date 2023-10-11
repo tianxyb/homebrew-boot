0ab056c2d4bf6c3098d7ecaef11b3d3ccdd224f1
tian.txy <tian.txy@alibaba-inc.com>
2023-10-11 11:17:21 +0800

    upgrade version

--- env-tools.rb
+++ env-tools.rb
@@ -1,9 +1,10 @@
 class EnvTools < Formula
   desc "dns over https resolve by aliyun/tencent"
-  url "https://github.com/tianxyb/fusiji-template.git", tag: "envinit"
+  url "https://github.com/tianxyb/fusiji-template.git", tag: "1.0.1"
   
   def install
     bin.install "boot/git-init"
   end
 
 end
+
