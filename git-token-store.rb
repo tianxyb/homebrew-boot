b7f44563c4db0fa02ff06d06ef71cc231aa85c5b
tian.txy <tian.txy@alibaba-inc.com>
2023-10-12 14:49:00 +0800

    rename env-tools to git-token-store

--- git-token-store.rb
+++ git-token-store.rb
@@ -1,9 +1,9 @@
-class EnvTools < Formula
+class GitTokenStore < Formula
   desc "dns over https resolve by aliyun/tencent"
   url "https://github.com/tianxyb/fusiji-template.git", tag: "1.0.1"
   
   def install
-    bin.install "boot/git-init"
+    bin.install "boot/git-token-store"
   end
 
 end
