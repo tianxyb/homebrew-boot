6e51914da08ce99f8e8665a270da9a058442c152
tian.txy <tian.txy@alibaba-inc.com>
2023-10-12 14:52:05 +0800

    update git-token-store version

--- git-token-store.rb
+++ git-token-store.rb
@@ -1,6 +1,6 @@
 class GitTokenStore < Formula
   desc "dns over https resolve by aliyun/tencent"
-  url "https://github.com/tianxyb/fusiji-template.git", tag: "1.0.1"
+  url "https://github.com/tianxyb/fusiji-template.git", tag: "1.0.2"
   
   def install
     bin.install "boot/git-token-store"
